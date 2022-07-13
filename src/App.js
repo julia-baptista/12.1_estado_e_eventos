21e2f9bf26dd283280ad1314e9b233d69e475d39
Julia Baptista <baptista.julia@yahoo.com.br>
2022-07-13 12:39:11 -0300

    Upload do projeto

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import colors from './data';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      inputValue: '',
+    }
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(e) {
+    this.setState({ inputValue: e.target.value })
+  }
+
+//   handleChange(e) {
+//     this.setState({ [e.target.name] : e.target.value });
+//  }
+
+// handleChange({target}) {
+
+//   const { name } = target
+//   const value = target.type === 'checkbox' ? target.checked : target.value
+//   this.setState({
+//   [name]: value
+//   })
+// }
+
+  render() {
+    const { inputValue } = this.state;
+    return (
+      <div className="App">
+        <ul>
+        {
+          colors
+            .filter((color) => color.name.includes(inputValue))
+            .map((color) => (
+              <li key={color.hex}>
+                <div className='square-color' style={ { background: color.hex, border: '1px solid black' } }></div>
+                {color.name}
+              </li>
+            ))
+        }
+        </ul>
+        <label htmlFor="search">
+        <input type="text" id="search" onChange={this.handleChange}/>
+        </label>
+      </div>
+    );
+  }
 }
 
 export default App;
